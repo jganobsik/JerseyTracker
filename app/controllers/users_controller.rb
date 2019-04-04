cd7496309b88911b282e72727881c5005f25b771
Joe Ganobsik <jganobsik@gmail.com>
2019-04-03 22:40:06 -0400

    moved login to user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
-
+    get '/login' do
+        erb :'sessions/login.html'
+    end
+    
+    post '/sessions' do
+    redirect  erb: '/jerseys/index'
+    end
 
 
 end
