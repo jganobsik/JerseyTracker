e609eebcd90134b27d250933c3e655c3f236741c
Joe Ganobsik <jganobsik@gmail.com>
2019-04-03 23:01:24 -0400

    added user table to migrations

--- Rakefile
+++ Rakefile
@@ -1,4 +1,7 @@
-require_relative '/config/environment'
+ENV["SINATRA_ENV"] ||= "development"
+
+
+require_relative './config/environment'
 require 'sinatra/activerecord/rake'
 
 task :console do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
+
+
     get '/login' do
         erb :'users/login'
     end
     
-    post '/sessions' do
-    redirect  erb: '/jerseys/index'
+    post '/login' do
+        user = User.find_by(:email => params[:email])
+        if user && user.authenticate(params[:password])
+           sessions[:email] = user.email  
+           redirect  erb: '/jerseys/index'
+        else
+            redirect_to '/signup'
+        end
     end
 
 
