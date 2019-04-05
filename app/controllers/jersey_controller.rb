adf621e653329d1beaef8779e16476ca45f691cb
Joe Ganobsik <jganobsik@gmail.com>
2019-04-04 23:21:34 -0400

    added index to jerseys, stubbed new

--- app/controllers/jersey_controller.rb
+++ app/controllers/jersey_controller.rb
@@ -1,7 +1,11 @@
 class JerseyController < ApplicationController
 
     get '/jerseys' do
-            #display jerseys
-    end
-
+        @jerseys = Jersey.all
+        erb :'jerseys/index'
+      end
+    
+      get "/jerseys/new" do
+        erb :jerseys/new'
+      end
 end
