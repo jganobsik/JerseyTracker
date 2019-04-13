2f8d6ce2831c0a3ed29be06bac61a837a5992eba
Joe Ganobsik <jganobsik@gmail.com>
2019-04-13 14:40:57 -0400

    fix signup post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,10 +44,10 @@ class UsersController < ApplicationController
     end
   
     post '/signup' do 
-      if params[:username] == "" || params[:password] == ""
+      if params[:email] == "" || params[:password] == ""
         redirect to '/signup'
       else
-        @user = User.create(:username => params[:username], :password => params[:password])
+        @user = User.create(:email => params[:email], :password => params[:password])
         session[:user_id] = @user.id
         redirect '/jerseys'
       end
