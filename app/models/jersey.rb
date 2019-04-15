c4bdb39f578f728c023cb5bed214901daa09c649
Joe Ganobsik <jganobsik@gmail.com>
2019-04-15 16:32:52 -0400

    add param validation to jersey model

--- app/models/jersey.rb
+++ app/models/jersey.rb
@@ -1,3 +1,7 @@
 class Jersey < ActiveRecord::Base
     belongs_to :user
+
+    def self.valid_params?(params)
+        return !params[:title].empty? && !params[:player_number].empty? && !params[:sport].empty? && !params[:player].empty?
+      end
 end
